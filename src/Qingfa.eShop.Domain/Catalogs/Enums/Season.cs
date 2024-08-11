6bdefbd052a61520392e623b91969e033593bd82
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-08-11 16:55:55 +0700

    feat(catalogs): add Season enum
    Introduced Season enum to categorize clothing items based on seasons: Spring, Summer, Fall, and Winter.
    
    - Added `Season` enum with predefined values for Spring, Summer, Fall, and Winter.
    - Updated related entities and logic to utilize the new `Season` enum.

---
+++ src/Qingfa.eShop.Domain/Catalogs/Enums/Season.cs
@@ -0,0 +1,12 @@
+﻿namespace QingFa.EShop.Domain.Catalogs.Enums;
+
+/// <summary>
+/// Represents a predefined set of seasons.
+/// </summary>
+public enum Season
+{
+    Spring,
+    Summer,
+    Fall,
+    Winter
+}
