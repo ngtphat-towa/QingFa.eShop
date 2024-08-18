2f66e4dd8672d49b0a4293e64b97a2b9742c0f79
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-08-18 14:55:19 +0700

    feat(domain): simlified models

---
+++ src/Qingfa.eShop.Domain/Core/IAuditable.cs
@@ -0,0 +1,10 @@
+﻿namespace QingFa.EShop.Domain.Common
+{
+    public interface IAuditable
+    {
+        public DateTime DateCreated { get;  set; } 
+        public string CreatedBy { get; protected set; } 
+        public DateTime LastModifiedDate { get; protected set; }
+        public string LastModifiedBy { get; protected set; }
+    }
+}
