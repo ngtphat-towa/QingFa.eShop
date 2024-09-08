2532cd6966eb4074c6e38ff4dc9cf71d1ea434a7
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-09-08 15:33:53 +0700

    feat(Application): add IApplicationDbProvider and IApplicationIdentityProvider

---
+++ src/Qingfa.eShop.Application/Core/Interfaces/IApplicationIdentityProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QingFa.EShop.Domain.Identities.Entities;
+using QingFa.EShop.Infrastructure.Identity.Entities.Roles;
+using QingFa.EShop.Infrastructure.Identity.Entities;
+using QingFa.EShop.Domain.Core.Repositories;
+
+namespace QingFa.EShop.Application.Core.Interfaces
+{
+    public interface IApplicationIdentityProvider : IUnitOfWork
+    {
+        DbSet<AppUser> Users { get; }
+        DbSet<Role> Roles { get; }
+        DbSet<RolePermission> RolePermissions { get; }
+        DbSet<Permission> Permissions { get; }
+        DbSet<RefreshToken> RefreshTokens { get; }
+    }
+}
